6c33f74d1a320af8bc77f4cf54c82a72763ea866
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-20 10:04:45 -0800

    Create tabs for about me card

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,12 +1,39 @@
 import "./About.css"
 import { Typography, Divider, Card, Avatar, Row, Col } from "antd";
 import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 
 export default function About () {
     
     const { Title } = Typography;
     const { Meta } = Card;
+
+    const tabList = [
+        {
+            key:"Description",
+            tab:"Description",
+        },
+        {
+            key:"Skills",
+            tab:"Skills",
+        },
+        {
+            key:"Hobbies",
+            tab:"Hobbies",
+        }
+    ]
+
+    const contentList = {
+        Description: <p>I am a full-stack developer with a passion in building innovative products to solve complex problems. With my experiences as a personal trainer and a coordinator in social services, I’ve developed strong communication and leadership skills, along with a relentless work ethic. I've always been passionate about finding creative solutions to navigate through a problem, which inspired me to pursue software engineering. I am excited to utilize my previous experiences and my passion for problem-solving to foster my growth as a software developer.</p>,
+        Skills: <p>skills</p>,
+        Hobbies: "hobby",
+    }
+    
+    const [activeTab, setActiveTab] = useState('Description')
+    function handleTabChange (key) {
+        setActiveTab(key)
+    }
 
     return (
         <>
@@ -17,12 +44,6 @@ export default function About () {
             lg={{span:16, offset:4}}
             >
                 <Card
-                // cover={
-                //     <img
-                //       alt="example"
-                //       src="https://i.imgur.com/LUR7gwH.jpg"
-                //     />
-                //   }
                 title={<><Avatar src="https://i.imgur.com/LUR7gwH.jpg"/>&nbsp;&nbsp;Full stack software developer</>}
                 actions = {[
                     <a className="about-github-container action-btns" href="https://github.com/allenaxie" target="_blank">
@@ -34,10 +55,10 @@ export default function About () {
                         <span className="action-text">LinkedIn</span>
                     </a>,
                 ]}
+                tabList={tabList}
+                onTabChange={(key) => handleTabChange(key)}
                 >
-                    <Meta
-                        description="I am a full-stack developer with a passion in building innovative products to solve complex problems. With my experiences as a personal trainer and a coordinator in social services, I’ve developed strong communication and leadership skills, along with a relentless work ethic. I’ve always been passionate about finding creative solutions to navigate through a problem, which inspired me to pursue software engineering. I am excited to utilize my previous experiences and my passion for problem-solving to foster my growth as a software developer."
-                    />
+                    {contentList[activeTab]}
 
                 </Card>
             </Col>
