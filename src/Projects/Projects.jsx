a3bbd641aaf40730b8f29a5ff0e662b204c1c2cf
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-20 12:03:51 -0800

    Add cryptohub to projects section

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import "./Projects.css"
-import { Typography, Divider, Card, Avatar, Row, Col, Tag, Button } from "antd";
+import { Typography, Divider, Card, Avatar, Row, Col, Tag, Button, Image } from "antd";
 import { GithubOutlined } from '@ant-design/icons';
 
 export default function Projects () {
@@ -11,10 +11,22 @@ export default function Projects () {
         <>
             <Title className="project-divider"> Projects</Title>
             <Row>
-                <Col>
-                    <Card>
-                        hey
-                    </Card>
+                <Col
+                xs={{span:22, offset:1}}
+                >
+                    <div
+                    style={{
+                        width:150,
+                        height:300
+                    }}
+                    className="cryptohub-project"
+                    >
+                        <div className="crypto-overlay">
+                            <h3 className="overlay-title">CryptoHub</h3>
+                            <Button href="https://ax-cryptohub.netlify.app/" target="_blank" rel="noreferrer"><p>Project</p></Button>
+                            <Button href="https://github.com/allenaxie/Cryptohub" target="_blank" rel="noreferrer"><p>Github</p></Button>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         </>
