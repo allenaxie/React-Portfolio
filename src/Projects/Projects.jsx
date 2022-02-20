19b44b076555cea694804672ca6506381face771
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-20 12:17:37 -0800

    Add beforeIFly to projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -13,6 +13,8 @@ export default function Projects () {
             <Row>
                 <Col
                 xs={{span:22, offset:1}}
+                md={{span:12, offset: 1}}
+
                 >
                     <div
                     style={{
@@ -25,6 +27,24 @@ export default function Projects () {
                             <h3 className="overlay-title">CryptoHub</h3>
                             <Button href="https://ax-cryptohub.netlify.app/" target="_blank" rel="noreferrer"><p>Project</p></Button>
                             <Button href="https://github.com/allenaxie/Cryptohub" target="_blank" rel="noreferrer"><p>Github</p></Button>
+                        </div>
+                    </div>
+                </Col>
+                <Col
+                xs={{span:22, offset:1}}
+                md={{span:12, offset: 1}}
+                >
+                    <div
+                    style={{
+                        width:150,
+                        height:300
+                    }}
+                    className="beforeIFly-project"
+                    >
+                        <div className="beforeIFly-overlay">
+                            <h3 className="overlay-title">Before I Fly</h3>
+                            <Button href="https://before-i-fly.herokuapp.com/" target="_blank" rel="noreferrer"><p>Project</p></Button>
+                            <Button href="https://github.com/allenaxie/beforeifly" target="_blank" rel="noreferrer"><p>Github</p></Button>
                         </div>
                     </div>
                 </Col>
